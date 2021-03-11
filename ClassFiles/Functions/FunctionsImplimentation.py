fe97ab3eff20c00826f6f778e758b726e7c83f79
minefarmer <pgoldfarm@yahoo.com>
2021-03-10 19:42:47 -0600

    dinner

---
+++ ClassFiles/Functions/FunctionsImplimentation.py
@@ -0,0 +1,31 @@
+## Functions
+
+
+# print("Welcome to the program, what is your name?")
+# name_result = input("Enter your response here -> ")
+# print(f"Your response was {name_result}")  # Your response was Enter your response here -> rich
+
+# print("What did you think of the food you ate today?")
+# food_result = input("Enter your response here -> ")
+# print(f"Your response was {food_result}")  # Your response was great
+
+# print("What tv show ending did you dislike the most ever?")
+# show_result = input("Enter your response here -> ")
+# print(f"Your response was {show_result}")  # Your response was got
+
+
+
+def get_input_from_user():
+    result = input("Enter your response here -> ")
+    print(f"Your response was {result}")
+
+print("Welcome to the program, what is your name?")  # Enter your response here -> rich
+get_input_from_user()
+
+print("What did you think of the food you ate today?")
+food_result = input("Enter your response here -> ")
+get_input_from_user  # Enter your response here -> great
+
+print("What tv show ending did you dislike the most ever?")
+show_result = input("Enter your response here -> ")
+get_input_from_user # Your response was Got
