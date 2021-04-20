82ae4a816a38ecc5f08eb3deb6dc3497b8326f0f
minefarmer <pgoldfarm@yahoo.com>
2021-04-20 00:00:10 -0500

    FileUse/ReadandWrite: good night

--- ClassFiles/FileUse/ReadandWrite.py
+++ ClassFiles/FileUse/ReadandWrite.py
@@ -1,11 +1,11 @@
 
-
-
-
-
-
-
-
-
+first_var, second_var, third_var = my_tuple
+print(third_var)
+ 
+when run gives me:
+Traceback (most recent call last):
+  File "/home/rich/Desktop/MatHub/MastrurPython/ClassFiles/PythonCrashCourse/Tuples.py", line 17, in <module>
+    first_var, second_var, third_var = my_tuple
+ValueError: not enough values to unpack (expected 3, got 2)
 
 
